b618d1d389819f79ebf384f1ebd5d880b21307ee
minh22091996 <tranbinhminh22091996@gmail.com>
2023-03-24 10:54:56 +0700

    Initial commit

---
+++ src/BubbleSort.java
@@ -0,0 +1,31 @@
+public class BubbleSort {
+    static int[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+    public static void bubbleSort(int[] list) {
+
+
+
+        boolean needNextPass = true;
+        long startTime = System.nanoTime();
+        for (int k = 1; k < list.length && needNextPass; k++) {
+            /* Array may be sorted and next pass not needed */
+            needNextPass = false;
+            for (int i = 0; i < list.length - k; i++) {
+                if (list[i] > list[i + 1]) {
+                    /* Swap list[i] with list[i + 1] */
+                    int temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
+
+                    needNextPass = true; /* Next pass still needed */
+                }
+            }
+        }
+        System.out.println("thoi gian chay cua sap xep noi bot la : " + (System.nanoTime()-startTime)+"nano giay");
+    }
+    public static void main(String[] args) {
+        bubbleSort(list);
+        for (int i = 0; i < list.length; i++)
+            System.out.print(list[i] + " ");
+    }
+
+}
